0c82de5f448e28b0b2de2c424c52872d8bb6df00
Hubert Stärk <hubert.staerk@gmail.com>
2024-12-16 22:29:06 +0100

    add prime

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common'
 import { Component } from '@angular/core'
 import { RouterOutlet } from '@angular/router'
+import { ButtonModule } from 'primeng/button'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, ButtonModule, RouterOutlet],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,17 @@
 import { ApplicationConfig } from '@angular/core'
 import { provideRouter } from '@angular/router'
+import { provideHttpClient } from '@angular/common/http'
+import {
+  provideTanStackQuery,
+  QueryClient,
+} from '@tanstack/angular-query-experimental'
 
 import { routes } from './app.routes'
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)],
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(),
+    provideTanStackQuery(new QueryClient()),
+  ],
 }
